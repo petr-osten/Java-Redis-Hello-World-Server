ffcf94c7d3e41e5c668791cfa36aee7d45710ea6
Vojtěch Kletečka <vojta.kletecka@gmail.com>
2021-07-20 15:13:44 +0200

    Removed jedis

--- src/main/java/org/jcg/springboot/redis/config/RedisConfig.java
+++ src/main/java/org/jcg/springboot/redis/config/RedisConfig.java
@@ -2,26 +2,19 @@ package org.jcg.springboot.redis.config;
  
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericToStringSerializer;
  
 // Configuration class to set up the Redis configuration.
 @Configuration
 public class RedisConfig {
- 
-    // Setting up the Jedis connection factory.
-    @Bean
-    JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
-    }
-      
+       
     // Setting up the Redis template object.
     @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
-        final RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
-        redisTemplate.setConnectionFactory(jedisConnectionFactory());
-        redisTemplate.setValueSerializer(new GenericToStringSerializer<Object>(Object.class));
-        return redisTemplate;
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        // Add some specific configuration here. Key serializers, etc.
+        return template;
     }
 }
